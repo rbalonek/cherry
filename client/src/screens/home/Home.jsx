8bc5bd98067b29502ef382711b66f690c0dfdb28
bob <robertbalonek@gmail.com>
2020-12-24 09:20:51 -0800

    Responsive Header

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../../components/header/Header";
 import "./Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="red">Hi!</h1>
+    <div className="home-container">
+      <Header />
     </div>
   );
 }
