68275195620a8dfa757576e33afa1681853571a4
bob <robertbalonek@gmail.com>
2020-12-27 15:03:06 -0800

    Splash Intro

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Switch, Route, useHistory } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "../screens/home/Home";
+import IntroVid from "../screens/IntroVid/IntroVid";
 
 export default function MainContainer() {
   return (
     <Switch>
-      <Route path="/">
+      <Route path="/home">
         <Home />
+      </Route>
+      <Route path="/">
+        <IntroVid />
       </Route>
     </Switch>
   );
