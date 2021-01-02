7ca6b6d3953d5c088ec0a1db2846ac8e6472fe50
bob <robertbalonek@gmail.com>
2021-01-02 13:19:03 -0800

    Underline About

--- client/src/components/OpenedMenu/OpenedMenu.js
+++ client/src/components/OpenedMenu/OpenedMenu.js
@@ -6,8 +6,10 @@ export default function OpenedMenu() {
     <div className="opened-menu-container">
       <h1 className="hamburger-open-text hover-animation">About</h1>
       <h1 className="hamburger-open-text hover-animation">Say Hi</h1>
-      <h1 className="hamburger-open-text hover-animation">LinkedIn</h1>
-      <h1 className="hamburger-open-text hover-animation">Instagram</h1>
+      <h1 className="hamburger-open-text hover-animation LinkedIn">LinkedIn</h1>
+      <h1 className="hamburger-open-text hover-animation Instagram">
+        Just Cuz
+      </h1>
       <h1 className="hamburger-open-text hover-animation">Twitter</h1>
     </div>
   );
