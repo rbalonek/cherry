94a238e569239df4047a6c336676b69252b4a488
bob <robertbalonek@gmail.com>
2021-01-02 11:55:06 -0800

    NEWHEADER

--- client/src/components/header/NewHeader/NewHeader.js
+++ client/src/components/header/NewHeader/NewHeader.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewHeader.css";
 
+import { Animated } from "react-animated-css";
+import logo from "../../../assets/logo/Cherry-Logo.png";
+
 export default function NewHeader() {
-  var Chi = document.getElementById("chi");
+  const [inView, setInView] = useState(true);
+  const Chi = document.getElementById("chi");
+
+  const DownArrowClick = () => {
+    Chi.scrollIntoView();
+    setInView(false);
+    setTimeout(() => {
+      setInView(true);
+    }, 1000);
+  };
 
   return (
-    <div className="new-header-container" onClick={() => Chi.scrollIntoView()}>
+    <div className="new-header-container" onClick={DownArrowClick}>
+      <div className="new-header-logo-container">
+        <img style={{ height: 50 }} alt="logo" src={logo} />
+      </div>
       <div className="new-header-text-container">
-        <p className="new-header-text">
-          In Option 2 the Cherry logo and hamburger only appear as the first
-          project is visible. Note: The video is a little too ethereal but just
-          to see how the color animation would work.
-        </p>
+        <Animated
+          animationIn="fadeIn"
+          animationOut="fadeOut"
+          animationInDuration={100}
+          isVisible={inView}
+        >
+          <p className="new-header-text">
+            We're Cherry and this is a short intro that changes. This version
+            has a small logo at the top that slides into position as a person
+            scrolls down. Here's an email link :{" "}
+            <span style={{ textDecoration: "underline" }}>Say hi.</span>
+          </p>
+        </Animated>
       </div>
     </div>
   );
