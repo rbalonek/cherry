052d65bdb8b19440fd0b0fb120b1067470b2cfdf
bob <robertbalonek@gmail.com>
2021-01-03 10:59:06 -0800

    Ternary Header

--- client/src/components/ScrollSnapContainer/NewScrollSnap/NewScrollSnap.js
+++ client/src/components/ScrollSnapContainer/NewScrollSnap/NewScrollSnap.js
@@ -1,24 +1,11 @@
-import React, { useState } from "react";
-import Header from "../../header/Header";
+import React from "react";
 import "./NewScrollSnap.css";
-import { Fade } from "react-awesome-reveal";
 
 import StickyHeader from "../../StickyHeader/StickyHeader";
-import HamburgerSticky from "../../StickyHeader/HamburgerSticky";
 import ProjectsBody from "../../ProjectsBody/ProjectsBody";
-import OpenedMenu from "../../OpenedMenu/OpenedMenu";
 import NewHeader from "../../header/NewHeader/NewHeader";
 
 export default function NewScrollSnap() {
-  // const [hamburgerClosed, hamburgerToggle] = useState("hamburger-closed");
-
-  // const toggleHamb = () => {
-  //   if (hamburgerClosed === "hamburger-closed") {
-  //     hamburgerToggle("hamburger-opened-sticky");
-  //   } else {
-  //     hamburgerToggle("hamburger-closed");
-  //   }
-  // };
   return (
     <div className="new-scrollsnap-container">
       <div id="header" className="new-header-container">
