affd8d48035d54204b908d4d017d7b9893ca5d86
bob <robertbalonek@gmail.com>
2021-01-03 09:21:14 -0800

    All sticky NavBar

--- client/src/components/ScrollSnapContainer/NewScrollSnap/NewScrollSnap.js
+++ client/src/components/ScrollSnapContainer/NewScrollSnap/NewScrollSnap.js
@@ -10,28 +10,18 @@ import OpenedMenu from "../../OpenedMenu/OpenedMenu";
 import NewHeader from "../../header/NewHeader/NewHeader";
 
 export default function NewScrollSnap() {
-  const [hamburgerClosed, hamburgerToggle] = useState("hamburger-closed");
+  // const [hamburgerClosed, hamburgerToggle] = useState("hamburger-closed");
 
-  const toggleHamb = () => {
-    if (hamburgerClosed === "hamburger-closed") {
-      hamburgerToggle("hamburger-opened-sticky");
-    } else {
-      hamburgerToggle("hamburger-closed");
-    }
-  };
+  // const toggleHamb = () => {
+  //   if (hamburgerClosed === "hamburger-closed") {
+  //     hamburgerToggle("hamburger-opened-sticky");
+  //   } else {
+  //     hamburgerToggle("hamburger-closed");
+  //   }
+  // };
   return (
     <div className="new-scrollsnap-container">
-      <div className={hamburgerClosed}>
-        <div className="hamburger-text-container">
-          <Fade>
-            <OpenedMenu />
-          </Fade>
-        </div>
-      </div>
       <StickyHeader />
-      <div className="hamburger-div" onClick={toggleHamb}>
-        <HamburgerSticky />
-      </div>
 
       <div id="header" className="new-header-container">
         <NewHeader />
--- client/src/components/StickyHeader/StickyHeader.js
+++ client/src/components/StickyHeader/StickyHeader.js
@@ -5,9 +5,20 @@ import { Bounce, Rotate, Zoom, Fade } from "react-awesome-reveal";
 import { Animated } from "react-animated-css";
 
 import Logo from "../../assets/logo/Cherry-Logo.png";
+import Hamburger from "../Hamburger/Hamburger";
+import OpenedMenu from "../OpenedMenu/OpenedMenu";
 
-export default function StickyHeader() {
+export default function StickyHeader({ toggle }) {
   // const [IsVis, setIsVis] = useState(true);
+  const [hamburgerClosed, hamburgerToggle] = useState("hamburger-closed");
+
+  const toggleHamb = () => {
+    if (hamburgerClosed === "hamburger-closed") {
+      hamburgerToggle("hamburger-opened-sticky");
+    } else {
+      hamburgerToggle("hamburger-closed");
+    }
+  };
 
   const logoClick = () => {
     window.scrollTo(0, 0);
@@ -19,7 +30,17 @@ export default function StickyHeader() {
 
   return (
     <div className="sticky-header-container">
+      <div className={hamburgerClosed}>
+        <div className="hamburger-text-container">
+          <Fade>
+            <OpenedMenu />
+          </Fade>
+        </div>
+      </div>
       <img className="logo-sticky" src={Logo} alt="logo" onClick={logoClick} />
+      <div onClick={toggleHamb}>
+        <Hamburger />
+      </div>
     </div>
   );
 }
