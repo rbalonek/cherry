4477a6019563fa5b633ab34282cca0927481dbe7
bob <robertbalonek@gmail.com>
2021-01-06 12:06:02 -0800

    Fade in Cherry Logo

--- client/src/components/header/NewHeader/NewHeader.js
+++ client/src/components/header/NewHeader/NewHeader.js
@@ -3,34 +3,34 @@ import "./NewHeader.css";
 import zenscroll from "zenscroll";
 
 import { Animated } from "react-animated-css";
+import { Fade } from "react-awesome-reveal";
 import { FiChevronDown } from "react-icons/fi";
 import axios from "axios";
 
 export default function NewHeader() {
   // const [inView, setInView] = useState(true);
-  const [fetchVideo, invokeFetchVideo] = useState([]);
+  // const [fetchVideo, invokeFetchVideo] = useState([]);
+  // const [visible, setVisible] = useState(true);
+  // const [classSet, setClassSet] = useState("fade-out-onLoad");
 
-  useEffect(() => {
-    const apiCall = async () => {
-      const data = await axios.get(
-        "https://api.airtable.com/v0/appVey7bH2bLRXZsC/VideoForLogo?view=Grid%20view",
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
-          },
-        }
-      );
-      invokeFetchVideo(data.data.records);
-      // console.log(fetchVideo[0].fields.link);
-    };
-    apiCall();
-  }, []);
+  // useEffect(() => {
+  //   const apiCall = async () => {
+  //     const data = await axios.get(
+  //       "https://api.airtable.com/v0/appVey7bH2bLRXZsC/VideoForLogo?view=Grid%20view",
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+  //         },
+  //       }
+  //     );
+  //     invokeFetchVideo(data.data.records);
+  //     // console.log(fetchVideo[0].fields.link);
+  //   };
+  //   apiCall();
+  // }, []);
+
   // const [imgHeight, newImgHeight] = useState("new-header-logo");
-
   // var chi = document.getElementById("chi");
-
-  //
-
   // const Chi = document.getElementById("chi");
 
   let pageHeight = window.innerHeight;
@@ -47,32 +47,42 @@ export default function NewHeader() {
     document.getElementById("chi").scrollIntoView();
     // setInView(false);
     // newImgHeight("new-header-logo-clicked");
-    setTimeout(() => {
-      // setInView(true);
-      // newImgHeight("new-header-logo");
-    }, 1000);
+    // setTimeout(() => {
+    //   setInView(true);
+    //   newImgHeight("new-header-logo");
+    // }, 1000);
   };
 
-  //   <div className="vid-logo-container">
-  //   <video
-  //     className="vid-about"
-  //     autoPlay
-  //     loop
-  //     muted
-  //     src={fetchVideo[0].fields.link}
-  //   />
-  //   <img
-  //     alt="logo"
-  //     src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1609886302/Cherry/Cherry%20Clients/Logos/Cherry_Logo_KO_dvmpzg.png"
-  //     className="about-logo"
-  //   />
-  // </div>
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     console.log("This will run after 3 seconds!");
+  //     setClassSet("fade-out");
+  //   }, 1000);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
     <>
       {window.innerWidth > 770 ? (
         <div>
-          <div className="new-header-container" onClick={DownArrowClick}></div>
+          <div className="new-header-container" onClick={DownArrowClick}>
+            <div className="vid-logo-container">
+              <Fade delay={1000}>
+                <video
+                  className="vid-about"
+                  autoPlay
+                  loop
+                  muted
+                  src="https://res.cloudinary.com/bobalobbadingdong/video/upload/v1609886317/Cherry/Cherry%20Clients/Logos/AdobeStock_326483459_Video_HD_Preview_vahjce.mp4"
+                />
+              </Fade>
+              <img
+                alt="logo"
+                src="https://res.cloudinary.com/bobalobbadingdong/image/upload/v1609959569/Cherry/Cherry%20Clients/Logos/Cherry_Logo_KO4_z46it6.png"
+                className="about-logo"
+              />
+            </div>
+          </div>
         </div>
       ) : (
         <div>
@@ -90,28 +100,3 @@ export default function NewHeader() {
     </>
   );
 }
-
-// <div className="new-header-text-container">
-//               {fetchQuotes.length ? (
-//                 <Animated
-//                   animationIn="fadeIn"
-//                   animationInDuration={3000}
-//                   animationOut="fadeOut"
-//                   animationOutDuration={300}
-//                   isVisible={inView}
-//                 >
-//                   <p className="new-header-text">
-//                     {fetchQuotes[0].fields.paragraph1}
-//                     <br />
-//                     <br />
-//                     {fetchQuotes[0].fields.paragraph2}
-//                     <br /> <br />
-//                     <span className="hi-email">
-//                       {fetchQuotes[0].fields.paragraph3}
-//                     </span>
-//                   </p>
-//                 </Animated>
-//               ) : (
-//                 <p></p>
-//               )}
-//             </div>
