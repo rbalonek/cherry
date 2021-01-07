68a4d861d005c76d26092b56d639005f86156786
bob <robertbalonek@gmail.com>
2021-01-07 09:48:29 -0800

    Background hamb menu open

--- client/src/components/OpenedMenu/OpenedMenu.js
+++ client/src/components/OpenedMenu/OpenedMenu.js
@@ -19,13 +19,20 @@ export default function OpenedMenu() {
 
   return (
     <div className="opened-menu-container">
+      <video
+        className="vid-about-opened-menu"
+        autoPlay
+        loop
+        muted
+        src="https://res.cloudinary.com/bobalobbadingdong/video/upload/v1609886317/Cherry/Cherry%20Clients/Logos/AdobeStock_326483459_Video_HD_Preview_vahjce.mp4"
+      />
       <h1 className=" hover-animation about" onClick={clickedAbout}>
         About
       </h1>
-      <h1 className=" hover-animation identity">Key Art</h1>
+      <h1 className=" hover-animation keyart">Key Art</h1>
       <h1 className=" hover-animation identity">Identity</h1>
       <h1 className=" hover-animation ">Package</h1>
-      <h1 className=" hover-animation about">Ideas</h1>
+      <h1 className=" hover-animation ideas">Ideas</h1>
       <h1 className=" hover-animation about">Say Hi</h1>
     </div>
   );
