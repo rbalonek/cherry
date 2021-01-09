d15c055e3ebd2be3f4ff3d2f722083e41c1d7545
bob <robertbalonek@gmail.com>
2021-01-08 17:26:52 -0800

    Menu Adjust

--- client/src/screens/Projects/ProjectsNavBar/ProjectsNavBar.js
+++ client/src/screens/Projects/ProjectsNavBar/ProjectsNavBar.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 export default function ProjectsNavBar() {
   const history = useHistory();
   return (
-    <div style={{ width: "100vw" }}>
+    <div style={{ width: "100vw", marginBottom: "20px", marginTop: "10px" }}>
       <div
         style={{
           display: "flex",
@@ -14,49 +14,46 @@ export default function ProjectsNavBar() {
         }}
       >
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/projects")}
         >
           All Projects
         </h3>
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/logos")}
         >
           LOGO
         </h3>
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/keyart")}
         >
           Key Art
         </h3>
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/identity")}
         >
           Identity
         </h3>
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/motiongfx")}
         >
           Motion GFX
         </h3>
-        <h3
-          style={{ color: "white", cursor: "pointer" }}
-          onClick={() => history.push("/copy")}
-        >
+        <h3 className="projects-nav-text" onClick={() => history.push("/copy")}>
           Copy
         </h3>
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/retouching")}
         >
           Retouching
         </h3>
         <h3
-          style={{ color: "white", cursor: "pointer" }}
+          className="projects-nav-text"
           onClick={() => history.push("/photoartdirection")}
         >
           Photo Art Direction
