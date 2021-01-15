00c2ae0aadc85cec1da7c63d82801510a479b369
bob <robertbalonek@gmail.com>
2021-01-15 11:41:53 -0800

    About screen Adj font

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-text-container">
+        <h3 className="footer-text">EMAIL</h3>
+        <h3 className="footer-text">|</h3>
+        <h3 className="footer-text">LINKEDIN</h3>
+        <h3 className="footer-text">|</h3>
+        <h3 className="footer-text">INSTAGRAM</h3>
+      </div>
+    </div>
+  );
+}
