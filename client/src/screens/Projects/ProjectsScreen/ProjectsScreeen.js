847e73647a449af49c4122082d74145dd5e21fdd
bob <robertbalonek@gmail.com>
2021-01-15 09:25:04 -0800

    Remove non images

--- client/src/screens/Projects/ProjectsScreen/ProjectsScreeen.js
+++ client/src/screens/Projects/ProjectsScreen/ProjectsScreeen.js
@@ -9,83 +9,16 @@ export default function ProjectsScreeen(props) {
   return (
     <div style={{ width: "100vw", minHeight: "100vh" }}>
       <ProjectsStickyHeader videoBackground={props.videoBackground} />
-      <div class="masonry">
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-        <Fade
-          direction={"up"}
-          fraction={Math.random()}
-          triggerOnce={true}
-          className="masonry-brick centered-text"
-        >
-          <div className="masonry-brick centered-text">Masonry Fixed 1</div>
-        </Fade>
-      </div>
 
       <div class="masonry-img">
         {props.projects.map((project) => (
           <>
             <Fade
               direction={"up"}
+              duration={2000}
               fraction={Math.random()}
               triggerOnce={true}
               className="masonry-brick-img"
-              x
             >
               <div
                 onMouseEnter={() => {
