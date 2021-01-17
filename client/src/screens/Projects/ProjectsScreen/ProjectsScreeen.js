9dbbf9010c195dd2d158402815cbfbb31b4bcf5c
bob <robertbalonek@gmail.com>
2021-01-17 10:29:10 -0800

    Projects in order and size from Airtable

--- client/src/screens/Projects/ProjectsScreen/ProjectsScreeen.js
+++ client/src/screens/Projects/ProjectsScreen/ProjectsScreeen.js
@@ -6,6 +6,8 @@ import { useHistory } from "react-router-dom";
 
 export default function ProjectsScreeen(props) {
   const history = useHistory();
+  // console.log(props);
+
   return (
     <div style={{ width: "100vw", minHeight: "100vh" }}>
       <ProjectsStickyHeader videoBackground={props.videoBackground} />
@@ -13,63 +15,185 @@ export default function ProjectsScreeen(props) {
       <div class="masonry-img">
         {props.projects.map((project) => (
           <>
-            <Fade
-              direction={"up"}
-              duration={2000}
-              fraction={Math.random()}
-              triggerOnce={true}
-              className="masonry-brick-img"
-            >
-              <div
-                onMouseEnter={() => {
-                  var el = document.getElementById(project.fields.id);
-                  el.classList.toggle("show-proj-info");
-                  el.classList.toggle("hide-element");
-                }}
-                onMouseLeave={() => {
-                  var el = document.getElementById(project.fields.id);
-                  el.classList.toggle("show-proj-info");
-                  el.classList.toggle("hide-element");
-                }}
-                class="masonry-brick-img"
-                style={{
-                  backgroundImage: `url(${project.fields.img1})`,
-                }}
+            {project.fields.mainPhotoLarge === true && (
+              <Fade
+                direction={"up"}
+                duration={2000}
+                fraction={Math.random()}
+                triggerOnce={true}
+                className="masonry-brick-img-full"
               >
                 <div
-                  id={project.fields.id}
-                  className="hide-element"
-                  onClick={() =>
-                    history.push(`/project/${project.fields.projectname}`)
-                  }
+                  onMouseEnter={() => {
+                    var el = document.getElementById(project.fields.id);
+                    el.classList.toggle("show-proj-info");
+                    el.classList.toggle("hide-element");
+                  }}
+                  onMouseLeave={() => {
+                    var el = document.getElementById(project.fields.id);
+                    el.classList.toggle("show-proj-info");
+                    el.classList.toggle("hide-element");
+                  }}
+                  class="masonry-brick-img-full"
+                  style={{
+                    backgroundImage: `url(${project.fields.img1})`,
+                  }}
                 >
-                  <Fade direction={"up"} duration={500} cascade={true}>
-                    <p
-                      style={{
-                        color: "white",
-                        fontFamily: "Rubik",
-                        fontWeight: "900",
-                        fontSize: "calc(15px + 1vw)",
-                      }}
-                    >
-                      {project.fields.projectname}
-                    </p>
+                  <div
+                    id={project.fields.id}
+                    className="hide-element"
+                    onClick={() =>
+                      history.push(`/project/${project.fields.projectname}`)
+                    }
+                  >
+                    <Fade direction={"up"} duration={500} cascade={true}>
+                      <p
+                        style={{
+                          color: "white",
+                          fontFamily: "Rubik",
+                          fontWeight: "900",
+                          fontSize: "calc(15px + 1vw)",
+                        }}
+                      >
+                        {project.fields.projectname}
+                      </p>
 
-                    <p
-                      style={{
-                        color: "white",
-                        fontFamily: "Rubik",
-                        fontWeight: "300",
-                        fontSize: "calc(5px + 1vw)",
-                        marginTop: "-30%",
-                      }}
-                    >
-                      {project.fields.client}
-                    </p>
-                  </Fade>
+                      <p
+                        style={{
+                          color: "white",
+                          fontFamily: "Rubik",
+                          fontWeight: "300",
+                          fontSize: "calc(5px + 1vw)",
+                          marginTop: "-30%",
+                        }}
+                      >
+                        {project.fields.client}
+                      </p>
+                    </Fade>
+                  </div>
                 </div>
-              </div>
-            </Fade>
+              </Fade>
+            )}
+
+            {project.fields.mainPhotoWide === true && (
+              <Fade
+                direction={"up"}
+                duration={2000}
+                fraction={Math.random()}
+                triggerOnce={true}
+                className="masonry-brick-img-wide"
+              >
+                <div
+                  onMouseEnter={() => {
+                    var el = document.getElementById(project.fields.id);
+                    el.classList.toggle("show-proj-info");
+                    el.classList.toggle("hide-element");
+                  }}
+                  onMouseLeave={() => {
+                    var el = document.getElementById(project.fields.id);
+                    el.classList.toggle("show-proj-info");
+                    el.classList.toggle("hide-element");
+                  }}
+                  class="masonry-brick-img-wide"
+                  style={{
+                    backgroundImage: `url(${project.fields.img1})`,
+                  }}
+                >
+                  <div
+                    id={project.fields.id}
+                    className="hide-element"
+                    onClick={() =>
+                      history.push(`/project/${project.fields.projectname}`)
+                    }
+                  >
+                    <Fade direction={"up"} duration={500} cascade={true}>
+                      <p
+                        style={{
+                          color: "white",
+                          fontFamily: "Rubik",
+                          fontWeight: "900",
+                          fontSize: "calc(15px + 1vw)",
+                        }}
+                      >
+                        {project.fields.projectname}
+                      </p>
+
+                      <p
+                        style={{
+                          color: "white",
+                          fontFamily: "Rubik",
+                          fontWeight: "300",
+                          fontSize: "calc(5px + 1vw)",
+                          marginTop: "-30%",
+                        }}
+                      >
+                        {project.fields.client}
+                      </p>
+                    </Fade>
+                  </div>
+                </div>
+              </Fade>
+            )}
+
+            {project.fields.mainPhotoSmall === true && (
+              <Fade
+                direction={"up"}
+                duration={2000}
+                fraction={Math.random()}
+                triggerOnce={true}
+                className="masonry-brick-img-sm"
+              >
+                <div
+                  onMouseEnter={() => {
+                    var el = document.getElementById(project.fields.id);
+                    el.classList.toggle("show-proj-info");
+                    el.classList.toggle("hide-element");
+                  }}
+                  onMouseLeave={() => {
+                    var el = document.getElementById(project.fields.id);
+                    el.classList.toggle("show-proj-info");
+                    el.classList.toggle("hide-element");
+                  }}
+                  class="masonry-brick-img-sm"
+                  style={{
+                    backgroundImage: `url(${project.fields.img1})`,
+                  }}
+                >
+                  <div
+                    id={project.fields.id}
+                    className="hide-element"
+                    onClick={() =>
+                      history.push(`/project/${project.fields.projectname}`)
+                    }
+                  >
+                    <Fade direction={"up"} duration={500} cascade={true}>
+                      <p
+                        style={{
+                          color: "white",
+                          fontFamily: "Rubik",
+                          fontWeight: "900",
+                          fontSize: "calc(15px + 1vw)",
+                        }}
+                      >
+                        {project.fields.projectname}
+                      </p>
+
+                      <p
+                        style={{
+                          color: "white",
+                          fontFamily: "Rubik",
+                          fontWeight: "300",
+                          fontSize: "calc(5px + 1vw)",
+                          marginTop: "-30%",
+                        }}
+                      >
+                        {project.fields.client}
+                      </p>
+                    </Fade>
+                  </div>
+                </div>
+              </Fade>
+            )}
           </>
         ))}
       </div>
