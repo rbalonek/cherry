8ccfba47e6cd8a8865dd9c1d33e0586f92aed9e1
bob <robertbalonek@gmail.com>
2021-01-19 11:49:44 -0800

    Design

--- client/src/screens/DesignScreen/Design.js
+++ client/src/screens/DesignScreen/Design.js
@@ -5,14 +5,15 @@ import { Fade } from "react-awesome-reveal";
 import { useHistory } from "react-router-dom";
 
 export default function Design(props) {
+  const history = useHistory();
   return (
     <div style={{ width: "100vw", minHeight: "100vh" }}>
       <ProjectsStickyHeader videoBackground={props.videoBackground} />
 
-      <div>
+      <div className="masonry-img">
         {props.projects.map((project) => (
           <>
-            {(project.fields.design === true) |
+            {(project.fields.Design === true) |
               (project.fields.BookDesign === true) |
               (project.fields.OutOfHome === true) |
               (project.fields.Apparel === true) |
@@ -21,57 +22,201 @@ export default function Design(props) {
               (project.fields.MotionGFX === true) |
               (project.fields.OpeningCredits === true) |
               (project.fields.Packaging === true) && (
-              <Fade
-                direction={"up"}
-                duration={2000}
-                fraction={Math.random()}
-                triggerOnce={true}
-                className="masonry-brick-img-full"
-              >
-                <div
-                  onMouseEnter={() => {
-                    var el = document.getElementById(project.fields.id);
-                    el.classList.toggle("show-proj-info");
-                    el.classList.toggle("hide-element");
-                  }}
-                  onMouseLeave={() => {
-                    var el = document.getElementById(project.fields.id);
-                    el.classList.toggle("show-proj-info");
-                    el.classList.toggle("hide-element");
-                  }}
-                  className="masonry-brick-img-full"
-                  style={{
-                    backgroundImage: `url(${project.fields.img1})`,
-                  }}
-                >
-                  <div id={project.fields.id} className="hide-element">
-                    <Fade direction={"up"} duration={500} cascade={true}>
-                      <p
+              <>
+                {project.fields.mainPhotoLarge === true && (
+                  <>
+                    <Fade
+                      direction={"up"}
+                      duration={2000}
+                      fraction={Math.random()}
+                      triggerOnce={true}
+                      className=" masonry-brick-img-full-key-art"
+                    >
+                      <div
+                        onMouseEnter={() => {
+                          var el = document.getElementById(project.fields.id);
+                          el.classList.toggle("show-proj-info");
+                          el.classList.toggle("hide-element");
+                        }}
+                        onMouseLeave={() => {
+                          var el = document.getElementById(project.fields.id);
+                          el.classList.toggle("show-proj-info");
+                          el.classList.toggle("hide-element");
+                        }}
+                        className=" masonry-brick-img-full-key-art"
                         style={{
-                          color: "white",
-                          fontFamily: "Rubik",
-                          fontWeight: "900",
-                          fontSize: "calc(15px + 1vw)",
+                          backgroundImage: `url(${project.fields.img1})`,
+                          // backgroundSize: "100% 100%",
+                          // backgroundPosition: "center",
+                          // objectFit: "cover",
+                          backgroundRepeat: "no-repeat",
                         }}
                       >
-                        {project.fields.projectname}
-                      </p>
+                        <div
+                          id={project.fields.id}
+                          className="hide-element"
+                          onClick={() =>
+                            history.push(
+                              `/project/${project.fields.projectname}`
+                            )
+                          }
+                        >
+                          <Fade direction={"up"} duration={500} cascade={true}>
+                            <p
+                              style={{
+                                color: "white",
+                                fontFamily: "Rubik",
+                                fontWeight: "900",
+                                fontSize: "calc(15px + 1vw)",
+                              }}
+                            >
+                              {project.fields.projectname}
+                            </p>
 
-                      <p
+                            <p
+                              style={{
+                                color: "white",
+                                fontFamily: "Rubik",
+                                fontWeight: "300",
+                                fontSize: "calc(5px + 1vw)",
+                                marginTop: "-30%",
+                              }}
+                            >
+                              {project.fields.client}
+                            </p>
+                          </Fade>
+                        </div>
+                      </div>
+                    </Fade>
+                  </>
+                )}
+
+                {project.fields.mainPhotoWide === true && (
+                  <>
+                    <Fade
+                      direction={"up"}
+                      duration={2000}
+                      fraction={Math.random()}
+                      triggerOnce={true}
+                      className="masonry-brick-img-wide-key-art"
+                    >
+                      <div
+                        onMouseEnter={() => {
+                          var el = document.getElementById(project.fields.id);
+                          el.classList.toggle("show-proj-info");
+                          el.classList.toggle("hide-element");
+                        }}
+                        onMouseLeave={() => {
+                          var el = document.getElementById(project.fields.id);
+                          el.classList.toggle("show-proj-info");
+                          el.classList.toggle("hide-element");
+                        }}
+                        onClick={() =>
+                          history.push(`/project/${project.fields.projectname}`)
+                        }
+                        className="masonry-brick-img-wide-key-art"
                         style={{
-                          color: "white",
-                          fontFamily: "Rubik",
-                          fontWeight: "300",
-                          fontSize: "calc(5px + 1vw)",
-                          marginTop: "-30%",
+                          backgroundImage: `url(${project.fields.img1})`,
+                          backgroundSize: "100% 100%",
+                          backgroundPosition: "center",
+                          objectFit: "cover",
+                          backgroundRepeat: "no-repeat",
                         }}
                       >
-                        {project.fields.client}
-                      </p>
+                        <div id={project.fields.id} className="hide-element">
+                          <Fade direction={"up"} duration={500} cascade={true}>
+                            <p
+                              style={{
+                                color: "white",
+                                fontFamily: "Rubik",
+                                fontWeight: "900",
+                                fontSize: "calc(15px + 1vw)",
+                              }}
+                            >
+                              {project.fields.projectname}
+                            </p>
+
+                            <p
+                              style={{
+                                color: "white",
+                                fontFamily: "Rubik",
+                                fontWeight: "300",
+                                fontSize: "calc(5px + 1vw)",
+                                marginTop: "-30%",
+                              }}
+                            >
+                              {project.fields.client}
+                            </p>
+                          </Fade>
+                        </div>
+                      </div>
                     </Fade>
-                  </div>
-                </div>
-              </Fade>
+                  </>
+                )}
+
+                {project.fields.mainPhotoSmall === true && (
+                  <>
+                    <Fade
+                      direction={"up"}
+                      duration={2000}
+                      fraction={Math.random()}
+                      triggerOnce={true}
+                      className="masonry-brick-img-sm-key-art"
+                    >
+                      <div
+                        onMouseEnter={() => {
+                          var el = document.getElementById(project.fields.id);
+                          el.classList.toggle("show-proj-info");
+                          el.classList.toggle("hide-element");
+                        }}
+                        onMouseLeave={() => {
+                          var el = document.getElementById(project.fields.id);
+                          el.classList.toggle("show-proj-info");
+                          el.classList.toggle("hide-element");
+                        }}
+                        onClick={() =>
+                          history.push(`/project/${project.fields.projectname}`)
+                        }
+                        className="masonry-brick-img-sm-key-art"
+                        style={{
+                          backgroundImage: `url(${project.fields.img1})`,
+                          // backgroundSize: "100% 100%",
+                          backgroundPosition: "center",
+                          objectFit: "cover",
+                          backgroundRepeat: "no-repeat",
+                        }}
+                      >
+                        <div id={project.fields.id} className="hide-element">
+                          <Fade direction={"up"} duration={500} cascade={true}>
+                            <p
+                              style={{
+                                color: "white",
+                                fontFamily: "Rubik",
+                                fontWeight: "900",
+                                fontSize: "calc(15px + 1vw)",
+                              }}
+                            >
+                              {project.fields.projectname}
+                            </p>
+
+                            <p
+                              style={{
+                                color: "white",
+                                fontFamily: "Rubik",
+                                fontWeight: "300",
+                                fontSize: "calc(5px + 1vw)",
+                                marginTop: "-30%",
+                              }}
+                            >
+                              {project.fields.client}
+                            </p>
+                          </Fade>
+                        </div>
+                      </div>
+                    </Fade>
+                  </>
+                )}
+              </>
             )}
           </>
         ))}
