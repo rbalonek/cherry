dc958bd86a36218430bbf3f436502e23659fd8e3
bob <robertbalonek@gmail.com>
2021-01-19 09:03:22 -0800

    NeedNewModal

--- client/src/screens/Projects/ProjName/ProjName.js
+++ client/src/screens/Projects/ProjName/ProjName.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectsStickyHeader from "../ProjectsStickyHeader/ProjectsStickyHeader";
 import "./ProjName.css";
 import { useHistory } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
+import { Animated } from "react-animated-css";
+import ModalProject from "../../../components/ModalProject/ModalProject";
 // import { AttentionSeeker } from "react-awesome-reveal";
 
 export default function ProjName(props) {
+  console.log(props);
   const history = useHistory();
+  const [modal, modalToggle] = useState("modalClosed");
+  const [modalText, modalTextToggle] = useState("modal-text-closed");
+
+  const toggleOpen = () => {
+    modalToggle("modalOpen clickable-modal");
+    modalTextToggle("modal-text-open");
+    // props.handleClickHamburger("show-none");
+    // props.handleClick("show-none");
+    // toggleLocked();
+  };
+
+  const toggleClosed = () => {
+    modalToggle("modalClosed");
+    modalTextToggle("modal-text-closed");
+    // props.handleClick("logo-sticky");
+    // props.handleClickHamburger("menu-btn");
+    // toggleLocked();
+  };
 
   let projName = window.location.pathname;
   let n = 9;
@@ -19,35 +40,77 @@ export default function ProjName(props) {
   props.projects.map(
     (project) => project.fields.projectname === projName && arr.push(project)
   );
-  console.log(arr);
+  // console.log(arr);
 
   return (
     <div>
       <div>
         <ProjectsStickyHeader videoBackground={props.videoBackground} />
       </div>
+
       <div className="holder">
-        <Carousel>
+        <Carousel
+          showStatus={false}
+          infiniteLoop
+          showIndicators={true}
+          showThumbs={false}
+          autoPlay={false}
+          useKeyboardArrows={true}
+          swipeable
+        >
           {arr.map((project) => (
             <>
               <div className="centering">
-                <img
-                  className="img-center"
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    // padding: "20px",
-                    textAlign: "center",
-                    backgroundSize: "cover",
-                    backgroundPosition: "50% 50%",
-                    backgroundRepeat: "no-repeat",
-                    // textAlign: "-webkit-center",
-                    // display: "flex",
-                    // justifyContent: "center",
-                  }}
-                  alt={project.fields.img1}
-                  src={project.fields.img1}
-                />
+                <div onClick={toggleOpen} className="clickable">
+                  <img
+                    className="img-center"
+                    style={{
+                      width: "100%",
+                      height: "100vh",
+
+                      // padding: "20px",
+                      textAlign: "center",
+                      objectFit: "cover",
+                      backgroundPosition: "50% 50%",
+                      backgroundRepeat: "no-repeat",
+                      // textAlign: "-webkit-center",
+                      // display: "flex",
+                      // justifyContent: "center",
+                    }}
+                    alt={project.fields.img1}
+                    src={project.fields.img1}
+                  />
+                </div>
+                <div className={modal} onClick={toggleClosed}></div>
+                <div className={modalText} onClick={toggleClosed}>
+                  <Animated
+                    animationIn="fadeIn"
+                    animationInDuration={2000}
+                    animationOut="fadeOut"
+                    animationOutDuration={100}
+                  >
+                    <ModalProject
+                      name={project.fields.name}
+                      client={project.fields.client}
+                      description={project.fields.description}
+                      descriptionTwo={project.fields.descriptionTwo}
+                      keyart={project.fields.keyart}
+                      Branding={project.fields.Branding}
+                      BookDesign={project.fields.BookDesign}
+                      OutOfHome={project.fields.OutOfHome}
+                      Apparel={project.fields.Apparel}
+                      Digital={project.fields.Digital}
+                      CustomType={project.fields.CustomType}
+                      MotionGFX={project.fields.MotionGFX}
+                      OpeningCredits={project.fields.OpeningCredits}
+                      Packaging={project.fields.Packaging}
+                      Copy={project.fields.Copy}
+                      Retouching={project.fields.Retouching}
+                      PhotoArtDirection={project.fields.PhotoArtDirection}
+                      BsidesPage={project.fields.BsidesPage}
+                    />
+                  </Animated>
+                </div>
               </div>
             </>
           ))}
