fb467ecaa99bf124de9b7424a6e02d06350c504c
bob <robertbalonek@gmail.com>
2021-01-19 08:23:13 -0800

    Modal Keywords

--- client/src/components/ModalProject/ModalProject.js
+++ client/src/components/ModalProject/ModalProject.js
@@ -6,6 +6,8 @@ export default function ModalProject(props) {
   const Name = props.name;
   const Client = props.client;
 
+  console.log(props);
+
   const history = useHistory();
   return (
     <div className="modal-container clickable-modal">
@@ -20,42 +22,141 @@ export default function ModalProject(props) {
         </div>
         <div className="what-did-container">
           <h2 className="bold-text">WHAT WE DID</h2>
-          <p
-            className="modal-text project-keywords"
-            onClick={() => history.push("/keyart")}
-          >
-            {props.keyart}
-          </p>
-          <p
-            className="modal-text project-keywords"
-            onClick={() => history.push("/identity")}
-          >
-            {props.identity}
-          </p>
-          <p
-            className="modal-text project-keywords"
-            onClick={() => history.push("/motiongfx")}
-          >
-            {props.motiongfx}
-          </p>
-          <p
-            className="modal-text project-keywords"
-            onClick={() => history.push("/copy")}
-          >
-            {props.copy}
-          </p>
-          <p
-            className="modal-text project-keywords"
-            onClick={() => history.push("/retouching")}
-          >
-            {props.retouching}
-          </p>
-          <p
-            className="modal-text project-keywords"
-            onClick={() => history.push("/photoartdirection")}
-          >
-            {props.photoartdirection}
-          </p>
+
+          {props.Logo === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/branding")}
+            >
+              Logos
+            </p>
+          )}
+
+          {props.keyart === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/keyart")}
+            >
+              Key Art
+            </p>
+          )}
+
+          {props.Branding === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/branding")}
+            >
+              Branding
+            </p>
+          )}
+
+          {props.BookDesign === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Book Design
+            </p>
+          )}
+
+          {props.OutOfHome === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Out of Home
+            </p>
+          )}
+
+          {props.Apparel === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Apparel
+            </p>
+          )}
+
+          {props.Digital === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Digital
+            </p>
+          )}
+
+          {props.CustomType === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Custom Type
+            </p>
+          )}
+
+          {props.MotionGFX === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Motion GFX
+            </p>
+          )}
+
+          {props.OpeningCredits === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Opening Credits
+            </p>
+          )}
+
+          {props.Packaging === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Packaging
+            </p>
+          )}
+
+          {props.PhotoArtDirection === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Photo Art Direction
+            </p>
+          )}
+
+          {props.Copy === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Copy
+            </p>
+          )}
+
+          {props.Retouching === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/design")}
+            >
+              Retouching
+            </p>
+          )}
+
+          {props.BsidesPage === true && (
+            <p
+              className="modal-text project-keywords"
+              onClick={() => history.push("/bsides")}
+            >
+              Unused Concepts
+            </p>
+          )}
         </div>
       </div>
     </div>
