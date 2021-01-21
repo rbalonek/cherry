aa36b02b507120c52fbecb726b3ce7a3bf22b087
bob <robertbalonek@gmail.com>
2021-01-21 15:40:14 -0800

    Arrows Working but huge margin top

--- client/src/components/ProjectsBody/ProjectsBody.jsx
+++ client/src/components/ProjectsBody/ProjectsBody.jsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./ProjectsBody.css";
 import axios from "axios";
 import zenscroll from "zenscroll";
-
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+  isSafari,
+} from "react-device-detect";
 import ClickableSlider from "../../slider/ClickableSlider/ClickableSlider";
 import ClickableSliderCell from "../../slider/ClickableSlider/ClickableSliderCell/ClickableSliderCell";
 
@@ -47,67 +53,134 @@ export default function ProjectsBody(props) {
   };
 
   return (
-    <div className="scroll-snap-container">
-      {fetchFullScreenProjects.map((project) => (
-        <div id={project.fields.idname} className="non-sticky-section">
-          {window.innerWidth > 770 ? (
-            <ClickableSlider
-              handleClick={props.handleClick}
-              handleClickHamburger={props.handleClickHamburger}
-              scrollToUp={project.fields.scrollUp}
-              scrollToDown={project.fields.scrollDown}
-              img1={project.fields.img1}
-              img2={project.fields.img2}
-              img3={project.fields.img3}
-              img4={project.fields.img4}
-              cellimg1={project.fields.cellimg1}
-              cellimg2={project.fields.cellimg2}
-              cellimg3={project.fields.cellimg3}
-              cellimg4={project.fields.cellimg4}
-              name={project.fields.name}
-              client={project.fields.client}
-              description={project.fields.description}
-              descriptionTwo={project.fields.descriptionTwo}
-              keyart={project.fields.keyart}
-              Branding={project.fields.Branding}
-              BookDesign={project.fields.BookDesign}
-              OutOfHome={project.fields.OutOfHome}
-              Apparel={project.fields.Apparel}
-              Digital={project.fields.Digital}
-              CustomType={project.fields.CustomType}
-              MotionGFX={project.fields.MotionGFX}
-              OpeningCredits={project.fields.OpeningCredits}
-              Packaging={project.fields.Packaging}
-              Copy={project.fields.Copy}
-              Retouching={project.fields.Retouching}
-              PhotoArtDirection={project.fields.PhotoArtDirection}
-              BsidesPage={project.fields.BsidesPage}
-            />
-          ) : (
-            <div>
-              <ClickableSliderCell
-                scrollToUp={project.fields.scrollUp}
-                scrollToDown={project.fields.scrollDown}
-                img1={project.fields.cellimg1}
-                img2={project.fields.cellimg2}
-                img3={project.fields.cellimg3}
-                img4={project.fields.cellimg4}
-                name={project.fields.name}
-                season={project.fields.season}
-                client={project.fields.client}
-                description={project.fields.description}
-                descriptionTwo={project.fields.descriptionTwo}
-                keyart={project.fields.keyart}
-                identity={project.fields.identity}
-                motiongfx={project.fields.motiongfx}
-                copy={project.fields.copy}
-                retouching={project.fields.retouching}
-                photoartdirection={project.fields.photoartdirection}
-              />
+    <>
+      {isSafari ? (
+        <div className="scroll-snap-container-safari">
+          {fetchFullScreenProjects.map((project) => (
+            <div id={project.fields.idname} className="non-sticky-section">
+              {window.innerWidth > 770 ? (
+                <ClickableSlider
+                  handleClick={props.handleClick}
+                  handleClickHamburger={props.handleClickHamburger}
+                  scrollToUp={project.fields.scrollUp}
+                  scrollToDown={project.fields.scrollDown}
+                  img1={project.fields.img1}
+                  img2={project.fields.img2}
+                  img3={project.fields.img3}
+                  img4={project.fields.img4}
+                  cellimg1={project.fields.cellimg1}
+                  cellimg2={project.fields.cellimg2}
+                  cellimg3={project.fields.cellimg3}
+                  cellimg4={project.fields.cellimg4}
+                  name={project.fields.name}
+                  client={project.fields.client}
+                  description={project.fields.description}
+                  descriptionTwo={project.fields.descriptionTwo}
+                  keyart={project.fields.keyart}
+                  Branding={project.fields.Branding}
+                  BookDesign={project.fields.BookDesign}
+                  OutOfHome={project.fields.OutOfHome}
+                  Apparel={project.fields.Apparel}
+                  Digital={project.fields.Digital}
+                  CustomType={project.fields.CustomType}
+                  MotionGFX={project.fields.MotionGFX}
+                  OpeningCredits={project.fields.OpeningCredits}
+                  Packaging={project.fields.Packaging}
+                  Copy={project.fields.Copy}
+                  Retouching={project.fields.Retouching}
+                  PhotoArtDirection={project.fields.PhotoArtDirection}
+                  BsidesPage={project.fields.BsidesPage}
+                />
+              ) : (
+                <div>
+                  <ClickableSliderCell
+                    scrollToUp={project.fields.scrollUp}
+                    scrollToDown={project.fields.scrollDown}
+                    img1={project.fields.cellimg1}
+                    img2={project.fields.cellimg2}
+                    img3={project.fields.cellimg3}
+                    img4={project.fields.cellimg4}
+                    name={project.fields.name}
+                    season={project.fields.season}
+                    client={project.fields.client}
+                    description={project.fields.description}
+                    descriptionTwo={project.fields.descriptionTwo}
+                    keyart={project.fields.keyart}
+                    identity={project.fields.identity}
+                    motiongfx={project.fields.motiongfx}
+                    copy={project.fields.copy}
+                    retouching={project.fields.retouching}
+                    photoartdirection={project.fields.photoartdirection}
+                  />
+                </div>
+              )}
             </div>
-          )}
+          ))}
         </div>
-      ))}
-    </div>
+      ) : (
+        <div className="scroll-snap-container">
+          {fetchFullScreenProjects.map((project) => (
+            <div id={project.fields.idname} className="non-sticky-section">
+              {window.innerWidth > 770 ? (
+                <ClickableSlider
+                  handleClick={props.handleClick}
+                  handleClickHamburger={props.handleClickHamburger}
+                  scrollToUp={project.fields.scrollUp}
+                  scrollToDown={project.fields.scrollDown}
+                  img1={project.fields.img1}
+                  img2={project.fields.img2}
+                  img3={project.fields.img3}
+                  img4={project.fields.img4}
+                  cellimg1={project.fields.cellimg1}
+                  cellimg2={project.fields.cellimg2}
+                  cellimg3={project.fields.cellimg3}
+                  cellimg4={project.fields.cellimg4}
+                  name={project.fields.name}
+                  client={project.fields.client}
+                  description={project.fields.description}
+                  descriptionTwo={project.fields.descriptionTwo}
+                  keyart={project.fields.keyart}
+                  Branding={project.fields.Branding}
+                  BookDesign={project.fields.BookDesign}
+                  OutOfHome={project.fields.OutOfHome}
+                  Apparel={project.fields.Apparel}
+                  Digital={project.fields.Digital}
+                  CustomType={project.fields.CustomType}
+                  MotionGFX={project.fields.MotionGFX}
+                  OpeningCredits={project.fields.OpeningCredits}
+                  Packaging={project.fields.Packaging}
+                  Copy={project.fields.Copy}
+                  Retouching={project.fields.Retouching}
+                  PhotoArtDirection={project.fields.PhotoArtDirection}
+                  BsidesPage={project.fields.BsidesPage}
+                />
+              ) : (
+                <div>
+                  <ClickableSliderCell
+                    scrollToUp={project.fields.scrollUp}
+                    scrollToDown={project.fields.scrollDown}
+                    img1={project.fields.cellimg1}
+                    img2={project.fields.cellimg2}
+                    img3={project.fields.cellimg3}
+                    img4={project.fields.cellimg4}
+                    name={project.fields.name}
+                    season={project.fields.season}
+                    client={project.fields.client}
+                    description={project.fields.description}
+                    descriptionTwo={project.fields.descriptionTwo}
+                    keyart={project.fields.keyart}
+                    identity={project.fields.identity}
+                    motiongfx={project.fields.motiongfx}
+                    copy={project.fields.copy}
+                    retouching={project.fields.retouching}
+                    photoartdirection={project.fields.photoartdirection}
+                  />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
