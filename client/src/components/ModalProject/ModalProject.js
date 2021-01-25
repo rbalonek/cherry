63d2f1e425f48ca79bce4a7bc960a91723b01159
bob <robertbalonek@gmail.com>
2021-01-25 09:57:49 -0800

    Sizing

--- client/src/components/ModalProject/ModalProject.js
+++ client/src/components/ModalProject/ModalProject.js
@@ -165,18 +165,3 @@ export default function ModalProject(props) {
     </div>
   );
 }
-
-// <div className="bold-container">
-//         <h2 className="bold-text">CLIENT</h2>
-//         <h2 className="bold-text">CHALLENGE</h2>
-//         <h2 className="bold-text">WHAT WE DID</h2>
-//       </div>
-//       <div className="props-container">
-//         <p className="modal-text client-name">{props.client}</p>
-//         <p className="modal-text project-name">{props.name}</p>
-//         <div className="description-text-container">
-//           <p className="description-text"> {props.description} </p>
-//           <br />
-//           <p className="description-text"> {props.descriptionTwo} </p>
-//         </div>
-//       </div>
